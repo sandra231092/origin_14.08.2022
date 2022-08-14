f5e1be1e88d17390ee6d47629447684934932e04
Alex <alex_bar2310@mail.ru>
2022-08-14 23:55:01 +0500

    task 11

---
+++ task11/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число = {number}");
+int digit1 = number / 100;
+int digit3 = number % 10;
+int res = digit1 * 10 + digit3;
+Console.WriteLine ($"Результат = {res}");

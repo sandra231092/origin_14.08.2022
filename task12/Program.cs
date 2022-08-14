0f6b5622f8ff6c09f018099869b9cdea95bc9f16
Alex <alex_bar2310@mail.ru>
2022-08-15 00:29:50 +0500

    task 12

---
+++ task12/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int mod = numberA % numberB;
+
+if (mod == 0)
+{
+    Console.Write("кратно");
+}
+else
+{
+    Console.Write($"не кратно, остаток {mod}");
+}

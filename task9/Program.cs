6944f17f475102baf3fa490b504dc78f63e04fdb
Alex <alex_bar2310@mail.ru>
2022-08-14 23:21:23 +0500

    task 9 was realized

---
+++ task9/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(10, 100);
+int digit2 = number % 10;
+int digit1 = number / 10;
+if (digit1 > digit2)
+{
+    Console.Write(digit1);
+}
+else
+{
+    Console.Write(digit2);
+}
+Console.WriteLine (" - наибольшая цифра");
